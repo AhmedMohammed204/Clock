e94b844d7ef04dcbf7c754d6339058cc91a01cbc
dell <dell@DESKTOP-OB7FVAR>
2023-11-29 14:11:54 +0300

    Add project files.

---
+++ Clock.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Clock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblCornClock = new System.Windows.Forms.Label();
+            this.lblMiddleClock = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblCornClock
+            // 
+            this.lblCornClock.AutoSize = true;
+            this.lblCornClock.Font = new System.Drawing.Font("Microsoft New Tai Lue", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCornClock.Location = new System.Drawing.Point(43, 30);
+            this.lblCornClock.Name = "lblCornClock";
+            this.lblCornClock.Size = new System.Drawing.Size(88, 25);
+            this.lblCornClock.TabIndex = 0;
+            this.lblCornClock.Text = "00:00:00";
+            // 
+            // lblMiddleClock
+            // 
+            this.lblMiddleClock.AutoSize = true;
+            this.lblMiddleClock.Font = new System.Drawing.Font("Microsoft New Tai Lue", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiddleClock.Location = new System.Drawing.Point(332, 219);
+            this.lblMiddleClock.Name = "lblMiddleClock";
+            this.lblMiddleClock.Size = new System.Drawing.Size(88, 25);
+            this.lblMiddleClock.TabIndex = 1;
+            this.lblMiddleClock.Text = "00:00:00";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft New Tai Lue", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(574, 30);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(163, 25);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "DD, mm dd, yyyy";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft New Tai Lue", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.Location = new System.Drawing.Point(386, 194);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(34, 25);
+            this.lbl5.TabIndex = 3;
+            this.lbl5.Text = "00";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Font = new System.Drawing.Font("Microsoft New Tai Lue", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6.Location = new System.Drawing.Point(398, 169);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(27, 20);
+            this.lbl6.TabIndex = 4;
+            this.lbl6.Text = "00";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7.Location = new System.Drawing.Point(419, 149);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(22, 17);
+            this.lbl7.TabIndex = 5;
+            this.lbl7.Text = "00";
+            // 
+            // lbl8
+            // 
+            this.lbl8.AutoSize = true;
+            this.lbl8.Font = new System.Drawing.Font("Microsoft New Tai Lue", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl8.Location = new System.Drawing.Point(447, 135);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(17, 13);
+            this.lbl8.TabIndex = 6;
+            this.lbl8.Text = "00";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft New Tai Lue", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(386, 244);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(34, 25);
+            this.lbl4.TabIndex = 7;
+            this.lbl4.Text = "00";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft New Tai Lue", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(398, 269);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(27, 20);
+            this.lbl3.TabIndex = 8;
+            this.lbl3.Text = "00";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(419, 289);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(22, 17);
+            this.lbl2.TabIndex = 9;
+            this.lbl2.Text = "00";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(447, 306);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(17, 13);
+            this.lbl1.TabIndex = 10;
+            this.lbl1.Tag = "Colored";
+            this.lbl1.Text = "00";
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 450);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblMiddleClock);
+            this.Controls.Add(this.lblCornClock);
+            this.Name = "Clock";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCornClock;
+        private System.Windows.Forms.Label lblMiddleClock;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ Clock.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Clock : Form
+    {
+        DateTime dt = new DateTime();
+        TimeSpan ts;
+        public Clock()
+        {
+            InitializeComponent();
+        }
+        enum enMonth
+        {
+            enJan = 1,
+            enFeb,
+            enMarch,
+            enAbr,
+            enMay,
+            enJun,
+            enJul,
+            enAug,
+            enSep,
+            enOct,
+            enNov,
+            enDes,
+        }
+        enMonth eMonth;
+        string GetMonth(enMonth Month)
+        {
+            switch (Month)
+            {
+                case enMonth.enJan:
+                    return "Jan";
+                case enMonth.enFeb:
+                    return "Feb";
+                case enMonth.enMarch:
+                    return "Mar";
+                case enMonth.enAbr:
+                    return "Abr";
+                case enMonth.enMay:
+                    return "May";
+                case enMonth.enJun:
+                    return "Jun";
+                case enMonth.enJul:
+                    return "Jul";
+                case enMonth.enAug:
+                    return "Aug";
+                case enMonth.enSep:
+                    return "Sep";
+                case enMonth.enOct:
+                    return "Oct";
+                case enMonth.enNov:
+                    return "Nov";
+                default:
+                    return "Des";
+
+
+            }
+        }
+        void MakeItColored(Label lbl, Label Prevlbl)
+        {
+            Prevlbl.Tag = "";
+            Prevlbl.ForeColor = Color.Black;
+            lbl.Tag = "Colored";
+            lbl.ForeColor = Color.DarkBlue;
+        }
+        void CheckColoredLableColored()
+        {
+            if (lbl1.Tag.ToString() == "Colored")
+            {
+                MakeItColored(lbl2, lbl1);
+                return;
+            }
+
+            if (lbl2.Tag.ToString() == "Colored")
+            {
+                MakeItColored(lbl3, lbl2);
+                return;
+            }
+
+            if (lbl3.Tag.ToString() == "Colored")
+            {
+                MakeItColored(lbl4, lbl3);
+                return;
+            }
+
+            if (lbl4.Tag.ToString() == "Colored")
+            {
+                MakeItColored(lblMiddleClock, lbl4);
+                return;
+            }
+
+
+            if (lblMiddleClock.Tag.ToString() == "Colored")
+            {
+                MakeItColored(lbl5,lblMiddleClock);
+                return;
+            }
+
+
+            if (lbl5.Tag.ToString() == "Colored")
+            {
+                MakeItColored(lbl6, lbl5);
+                return;
+            }
+
+            if (lbl6.Tag.ToString() == "Colored")
+            {
+                MakeItColored(lbl7, lbl6);
+                return;
+            }
+
+            if (lbl7.Tag.ToString() == "Colored")
+            {
+                MakeItColored(lbl8, lbl7);
+                return;
+            }
+            if (lbl8.Tag.ToString() == "Colored")
+            {
+                MakeItColored(lbl1, lbl8);
+                return;
+            }
+
+        }
+        void UpdatSecLabel(Label lbl, TimeSpan ts, byte Value, bool IsLabelAboveClock = true)
+        {
+            if (IsLabelAboveClock)
+            {
+                lbl.Text = (ts = new TimeSpan(ts.Hours, ts.Minutes,ts.Seconds + Value)).Seconds.ToString();
+                return;
+            }
+
+            lbl.Text = (ts = new TimeSpan(ts.Hours, ts.Minutes, ts.Seconds - Value)).Seconds.ToString();
+
+        }
+        void UpdateSecondsLables(TimeSpan ts)
+        {
+            UpdatSecLabel(lbl1, ts, 4, false);
+            UpdatSecLabel(lbl2, ts, 3, false);
+            UpdatSecLabel(lbl3, ts, 2, false);
+            UpdatSecLabel(lbl4, ts, 1, false);
+
+            UpdatSecLabel(lbl5, ts, 1);
+            UpdatSecLabel(lbl6, ts, 2);
+            UpdatSecLabel(lbl7, ts, 3);
+            UpdatSecLabel(lbl8, ts, 4);
+
+
+            CheckColoredLableColored();
+        }
+        void UpdateTime()
+        {
+            dt = DateTime.Now;
+            ts = new TimeSpan(dt.Hour, dt.Minute, dt.Second);
+
+
+            lblMiddleClock.Text = ts + (dt.Hour >= 12 ? " PM" : " AM");
+            lblCornClock.Text = lblMiddleClock.Text;
+
+
+            lblDate.Text = dt.DayOfWeek.ToString()
+                         + ", " + GetMonth((enMonth)dt.Month)
+                         + " " + dt.Day
+                         + ", " + dt.Year;
+
+            UpdateSecondsLables(ts);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateTime();
+        }
+    }
+}
